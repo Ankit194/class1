79d3a7ee4cb85cc527bab7f3915dff886417fd4e
ankit194 <aaankitagrawal19@gmail.com>
2018-04-10 00:08:31 +0530

    test3

--- data structures.R
+++ data structures.R
@@ -23,9 +23,36 @@ v5
 
 #Matrix----
 
+(mymatrix = matrix(1:24, ncol=6))
+mymatrix=matrix(1:100, ncol=10)
+
+mymatrix
+?matix
+
+mymatrix[2:3,3:4]
+
+rn=paste('R',1:10, sep='_')
+rn
+cn=paste('C',1:10, sep='_')
+cn
+dimnames(mymatrix)= list(c(rn), c(cn))
+mymatrix
+
+colSums(mymatrix)
+
+mymatrix1 = matrix(1:50,ncol=5)
+mymatrix1
+mymatrix2 = matrix(51:100,ncol=5)
+mymatrix2
 
 #Arrays----
 
+(myarray=array(1:100, dim=c(10,5,2)))
+
+(myarray=array(1:100, dim=c(10,5,2),dimnames=list(c('s1','s2','s3','s4','s5','s6','s7','s8','s9','s10'),c('sub1','sub2','sub3','sub4','sub5'))))
+
+apply(myarray,1,sum)
+apply(myarray,colSum)
 
 #List----
 
@@ -59,14 +86,28 @@ str(students)
 summary(students)
 
 
+name = c('s1','s2','s3','s4','s5')
+course=c('MTECH','BTECH','PHD','BTECH','MTECH')
+gender=c('M','F','M','F','M')
+grades=c('A','B','C','B','A')
+marks=c(runif(5,50,100))
+?runif
+df=data.frame(name,course,gender,grades,marks)
+df
+str(df)
+df$grades
+df[,1:3]
+df[1:2,]
 
 
+L1=letters[1:26]
+L1
 
+sample(L1,30, replace=T)
+set.seed(12)
+sample(L1,5)
+set.seed(12)
+sample(L1,5)
 
-
-
-
-
-
-
+         
 
